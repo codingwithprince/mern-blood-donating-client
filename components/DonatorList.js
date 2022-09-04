a40f93724932e8ffa0bec87cc858aeaf84122f18
Prince Ahmed <codingwithprince@gmail.com>
2022-09-04 23:14:13 +0600

    added

--- components/DonatorList.js
+++ components/DonatorList.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Moment from "react-moment";
+import "moment-timezone";
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import { IoAddCircleSharp } from "react-icons/io5";
@@ -9,10 +12,20 @@ import Spinner from "./Spinner";
 
 const DonatorList = () => {
   const [allDonators, setAllDonators] = useState();
-
+  const router = useRouter();
   useEffect(() => {
     axios.get("http://localhost:8080/").then((res) => setAllDonators(res.data));
   }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8080/delete/${id}`);
+      console.log("deleted");
+      console.log(id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="donator-list md:px-[20%] px-3">
@@ -25,7 +38,7 @@ const DonatorList = () => {
             Donators
           </h2>
           <table className="table text-center table-auto w-full shadow-lg">
-            <thead className="bg-slate-200 text-zinc-800">
+            <thead className="bg-red-200 text-zinc-800">
               <tr className="text-xs md:text-lg font-semibold">
                 <td className="p-2">Id</td>
                 <td className="p-2">Name</td>
@@ -39,21 +52,25 @@ const DonatorList = () => {
                 allDonators.map((item, index) => (
                   <tr
                     key={item._id}
-                    className="text-xs md:text-lg border-b-1 text-zinc-700  even:bg-zinc-100 hover:bg-zinc-100 odd:bg-white"
+                    className="text-xs md:text-lg border-b-1 text-zinc-700  even:bg-red-50 hover:bg-red-50 odd:bg-white"
                   >
                     <td className="p-2  border-gray-500"> {index + 1}</td>
-                    <td className="p-2"> {item.name} </td>
+                    <td className="p-2 hover:underline hover:text-blue-500">
+                      <Link href={`/info/${item._id}`}>{item.name}</Link>
+                    </td>
                     <td className="p-2 ">{item.bGroup} </td>
-                    <td className="p-2 "> {item.lastDonated} </td>
+                    <td className="p-2 ">
+                      <Moment format="DD/MM/YYYY">{item.lastDonated}</Moment>
+                    </td>
                     <td className="p-2 ">
                       <div className="icons flex gap-1 justify-center">
                         <Link href={`/update/${item._id}`}>
                           <FaEdit className="text-zinc-600" />
                         </Link>
-                        <AiFillDelete className="text-red-500" />
-                        <Link href={`/info/${item._id}`}>
-                          <FcInfo />
-                        </Link>
+                        <AiFillDelete
+                          onClick={() => handleDelete(item._id)}
+                          className="text-red-500"
+                        />
                       </div>
                     </td>
                   </tr>
--- components/SingleInfo.js
+++ components/SingleInfo.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Moment from 'react-moment';
+import 'moment-timezone';
 import { BsPersonBoundingBox } from 'react-icons/bs' 
 import axios from 'axios'
 import Spinner from './Spinner'
@@ -19,7 +21,7 @@ const SingleInfo = () => {
     <div className='md:px-[20%] px-3 py-5'>
         {
             singleDonator == null ? <Spinner /> : 
-            <div className='md:w-[500px] border-2 text-center shadow-xl rounded-md px-2 md:py-10 my-6 mx-auto'>
+            <div className='md:w-[500px] border-2 text-center shadow-xl rounded-md px-2 md:py-10 py-5 my-6 mx-auto'>
                 <div className="avatar flex justify-center">
                     <BsPersonBoundingBox size={100} className="text-red-500" />
                 </div>
@@ -28,7 +30,11 @@ const SingleInfo = () => {
                     <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Age : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.age}</span></h3>
                     <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Address : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.address}</span></h3>
                     <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Phone : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.phone}</span></h3>
-                    <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Last Donated : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.lastDonated}</span></h3>
+                    <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Last Donated : <span className='text-zinc-500 font-normal uppercase'>
+                        <Moment format="DD/MM/YYYY">
+                            {singleDonator.lastDonated}
+                        </Moment>
+                    </span></h3>
                     <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Blood Group : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.bGroup}</span></h3>
                     <h3 className='md:text-lg text-sm px-2 text-red-500 even:white odd:bg-yellow-50 rounded-lg'>Gender : <span className='text-zinc-500 font-normal uppercase'>{singleDonator.gender}</span></h3>
                 </div>
